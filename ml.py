8e2e28f08de7d26d8bf4ab5ecb5c747947b19db0
developersTogether <deepanshu.mishra0@gmail.com>
2018-09-18 19:11:42 +0530

    Prediction Stage

--- ml.py
+++ ml.py
@@ -57,7 +57,7 @@ dataset = pandas.read_csv(url, names=names)
 
 # Dataset Summary
 # print(dataset.shape)
-print(dataset.head(20))
+# print(dataset.head(20))
 
 # Statistical Summary
 # print(dataset.describe())
@@ -79,5 +79,55 @@ print(dataset.head(20))
 # Multivariate
 
 # Scatter Plot Matrix
-scatter_matrix(dataset)
-plt.show()
+# scatter_matrix(dataset)
+# plt.show()
+
+# Evaluation Algorithm
+array = dataset.values
+X = array[:, 0:4]
+Y = array[:, 4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,
+                                                                                    random_state=seed)
+
+# Test Options and Evaluation Metric
+seed = 7
+scoring = 'accuracy'
+
+# Spot Check Algorithms
+models = list()
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# Evaluate Each Model in Turn
+# results = []
+# names = []
+# for name, model in models:
+#     kfold = model_selection.KFold(n_splits=10, random_state=seed)
+#     cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+#     results.append(cv_results)
+#     names.append(name)
+#     msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+#     print(msg)
+
+
+# Compare Algorithms
+# fig = plt.figure()
+# fig.suptitle('Algorithm Comparison')
+# ax = fig.add_subplot(111)
+# plt.boxplot(results)
+# ax.set_xticklabels(names)
+# plt.show()
+
+# Make predictions on validation dataset
+svm = SVC()
+svm.fit(X_train, Y_train)
+predictions = svm.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
